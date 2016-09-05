4936f2312ecfb96a0f076c256409716cd0512073
jjchambl <jjchambliss@gmail.com>
2016-09-05 10:52:11 -0500

    repeat and directives add, todos add

--- scripts/app.js
+++ scripts/app.js
@@ -3,5 +3,18 @@ angular.module("todoApp", [])
 .controller('mainCtrl', function($scope) {
 	$scope.helloWorld = function() {
 		console.log("Hello from the controller!");
+	};
+
+	$scope.todos = [
+		{"name": "make food",},
+		{"name": "eat food"},
+		{"name": "finish all food"}
+	];
+})
+
+// Constructed to demo the use of child/parent scope
+.controller('demoCtrl', function($scope) {
+	$scope.hello = function() {
+		console.log("Hello from the demo controller!");
 	}
 });
